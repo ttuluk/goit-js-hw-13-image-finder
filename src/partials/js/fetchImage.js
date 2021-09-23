75bdd81155e41ce1d0c427527fcaf4b6e7b38985
ttuluk <ttasya.k@gmail.com>
2021-09-23 22:27:57 +0300

    add fetch

---
+++ src/partials/js/fetchImage.js
@@ -0,0 +1,36 @@
+// import galerryCard from '../../templates/image-card.hbs';
+// import form from '../../templates/form-search.hbs';
+
+import getRefs from './get-refs.js';
+const refs = getRefs();
+// https://pixabay.com/api/
+// ?image_type = photo
+//     &orientation=horizontal
+//     &q=что_искать
+//     &page=номер_страницы
+//     &per_page=12
+//         &key=твой_ключ
+
+const BASE_URL = `https://pixabay.com/api/`;
+const API_KEY = '23526463-69d460dc9366e545ff49935bb';
+
+const options = {
+    headers: {
+        Autorization: API_KEY,
+    }
+};
+
+let value = 'yellow+flowers';
+let type = 'photo';
+let orientation = 'horizontal';
+let page = 1;
+let perPage = 12;
+
+// let params = `?image_type${type}&orientation=${orientation}&q=${value}&page=${page}&per_page=${perPage}&key=${API_KEY}`;
+let params = `?key=${API_KEY}&q=${value}&image_type=${type}&orientation=${orientation}&page=${page}&per_page=${perPage}`;
+let url = BASE_URL + params;
+
+fetch(url).then((response) => { return response.json() }).then((data) => {
+    console.log(data);
+});
+
---
+++ src/partials/js/get-refs.js
@@ -0,0 +1,6 @@
+export default function getRefs() {
+  return {
+    cardContainer: document.querySelector('.js-galerry'),
+    formContainer: document.getElementById('form'),
+  };
+}
