5ba3cd79db1407df3bc5cf964c5f3b8d8ac4647a
ttuluk <ttasya.k@gmail.com>
2021-09-24 00:07:54 +0300

    add submit

--- src/partials/js/fetchImage.js
+++ src/partials/js/fetchImage.js
@@ -1,10 +1,11 @@
-// import galerryCard from '../../templates/image-card.hbs';
+import galerryCard from '../templates/image-card.hbs';
 // import form from '../templates/form-search.hbs';
 console.log(form);
 
 import getRefs from './get-refs.js';
 const refs = getRefs();
 console.log(refs.btnLoadMore);
+console.dir(refs.formContainer);
 // https://pixabay.com/api/
 // ?image_type = photo
 //     &orientation=horizontal
@@ -23,6 +24,7 @@ const options = {
 };
 
 let value = 'yellow+flowers';
+const nameV = 'car';
 let type = 'photo';
 let orientation = 'horizontal';
 let page = 1;
@@ -32,15 +34,41 @@ let perPage = 12;
 
 
 // refs.formContainer.insertAdjacentHTML('beforebegin', form);
-
+refs.formContainer.addEventListener('submit', getValue);
 refs.btnLoadMore.addEventListener('click', getFetch);
 
-function getFetch (e) {
-    console.log(e);
+function getFetch () {
     page += 1;
     let params = `?key=${API_KEY}&q=${value}&image_type=${type}&orientation=${orientation}&page=${page}&per_page=${perPage}`;
     let url = BASE_URL + params;
     fetch(url).then((response) => { return response.json() }).then((data) => {
-        console.log(data);
-    });
+      console.log(data);;
+    }).then((d) => console.log(d));
 };
+
+function getValue(e) {
+    console.log(e);
+    e.preventDefault();
+
+    // const inputName = e.target.input.value;
+    // console.log(inputName);
+page += 1;
+    let params = `?key=${API_KEY}&q=${nameV}&image_type=${type}&orientation=${orientation}&page=${page}&per_page=${perPage}`;
+    let url = BASE_URL + params;
+    console.log(url);
+    console.log();
+    fetch(url).then((response) => { return response.json() }).then((data) => {
+        return data.hits;
+    }).then((ar) => {
+        ar.map((elem) => {
+            console.log(elem);
+            return refs.cardContainer.innerHTML = galerryCard;
+//             webformatURL - ссылка на маленькое изображение для списка карточек
+// largeImageURL - ссылка на большое изображение (смотри пункт 'дополнительно')
+// likes - количество лайков
+// views - количество просмотров
+// comments - количество комментариев
+// downloads - количество загрузок
+        })}
+        );
+}
